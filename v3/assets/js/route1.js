9da8b24209972c8a6703940b0f1762e756515fdd
Acaroux <quentinsom@hotmail.fr>
2018-10-28 13:24:06 +0100

    road to route 1

---
+++ v3/assets/js/route1.js
@@ -0,0 +1,20 @@
+var map;
+
+
+class Route1 extends Phaser.Scene {
+    constructor() {
+        super({ key: "Route1"});
+    }
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+    update() {
+        
+    }
+}
